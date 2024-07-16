9f86c55293aa1fb3769361c4eac5e0967d871157
Jeremy Watt <jermwatt@gmail.com>
2024-07-16 15:04:20 -0700

    tabs re-added

--- bleep_that_sht/gradio_app_url_download.py
+++ bleep_that_sht/gradio_app_url_download.py
@@ -40,7 +40,7 @@ with gr.Blocks(theme=gr.themes.Soft(), title="🎬 Bleep That Sh*t 🙊") as dem
                         value="treetz, ice, cream, chocolate, syrup, cookie, hooked, threats, treats, trees",
                     )
                 with gr.Column(scale=3):
-                    gr.Dropdown(choices=avaliable_models, value=1, label="whisper model (base only in HF space)", info="whisper model selection")
+                    gr.Dropdown(choices=avaliable_models, value="base", label="whisper model (base only in HF space)", info="whisper model selection")
                 with gr.Column(scale=4):
                     just_transcribe_button = gr.Button("Just Transcribe", variant="primary")
                     bleep_transcribe_button = gr.Button("Transcribe & Bleep", variant="primary")
@@ -51,21 +51,21 @@ with gr.Blocks(theme=gr.themes.Soft(), title="🎬 Bleep That Sh*t 🙊") as dem
 
                 with gr.Row():
                     og_video = gr.Video(
+                        visible=False,
                         show_download_button=True,
                         show_label=True,
                         label="original video",
                         format="mp4",
-                        visible=True,
                         width="50vw",
                         height="50vw",
                     )
                                 
                     bleep_video = gr.Video(
+                        visible=False,
                         show_download_button=True,
                         show_label=True,
                         label="bleeped video",
                         format="mp4",
-                        visible=True,
                         width="50vw",
                         height="50vw",
                     )
@@ -81,137 +81,34 @@ with gr.Blocks(theme=gr.themes.Soft(), title="🎬 Bleep That Sh*t 🙊") as dem
                             
                     new_video = gr.Video(
                         value=temporary_video_location,
+                        visible=True,
                         show_download_button=True,
                         show_label=True,
                         label="original video",
                         format="mp4",
-                        visible=True,
                         width="50vw",
                         height="50vw",
                     )
                             
                     transcript_output = ""
                     return new_video, transcript_output
-
-
-    with gr.Tab("💡 About"):
-        with gr.Blocks() as about:
-            gr.Markdown(
-                (
-                    "### Bleep out words of your choice from an input video.  \n"
-                    "How it works: \n\n"
-                    "1.  Provided a youtube / shorts url \n"
-                    "2.  Choose your your desired bleep keywords \n"
-                    "3.  (if running locally) Choose a model from the Whisper family to transcribe the audio (defaults to base only for HF space) \n"
-                    "4.  (optional) Press 'Just Transcribe' to examine / download just the transcription of the video (can help in choosing bleep words) \n"
-                    "5.  Press 'Transcribe and bleep' to transcribe and replace all instances of your keywords with *beep* sounds \n\n"
-                    "If you want to select your Whisper model / run longer videos pull and run the app locally. \n\n"
-                    "Notice: baseline (not fine tuned) Whisper models are used here - you may need to be creative to bleep out all the versions of an input word you want depending on its transcription. \n\n"
-                    "You do *not* need a GPU to run this locally.  Larger models take more time to process locally, but its doable. \n"
+                    
+        with gr.TabItem("💡 About"):
+            with gr.Blocks() as about:
+                gr.Markdown(
+                    (
+                        "### Bleep out words of your choice from an input video.  \n"
+                        "How it works: \n\n"
+                        "1.  Provided a youtube / shorts url \n"
+                        "2.  Choose your your desired bleep keywords \n"
+                        "3.  (if running locally) Choose a model from the Whisper family to transcribe the audio (defaults to base only for HF space) \n"
+                        "4.  (optional) Press 'Just Transcribe' to examine / download just the transcription of the video (can help in choosing bleep words) \n"
+                        "5.  Press 'Transcribe and bleep' to transcribe and replace all instances of your keywords with *beep* sounds \n\n"
+                        "If you want to select your Whisper model / run longer videos pull and run the app locally. \n\n"
+                        "Notice: baseline (not fine tuned) Whisper models are used here - you may need to be creative to bleep out all the versions of an input word you want depending on its transcription. \n\n"
+                        "You do *not* need a GPU to run this locally.  Larger models take more time to process locally, but its doable. \n"
+                    )
                 )
-            )
-
-        # print(f"local_file_path --> {local_file_path}")
-
-        # if local_file_path is not None:
-        #     og_video = gr.PlayableVideo(
-        #         value=local_file_path,
-        #         show_download_button=True,
-        #         show_label=True,
-        #         label="bleeped video",
-        #         visible=True
-        #     )
-
-        # with gr.Row():
-        #     with gr.Column():
-        #         bleep_words = gr.Textbox(
-        #             placeholder="bleep keywords go here separated by commas",
-        #             label="bleep-word list",
-        #             value="treetz, ice, cream, chocolate, syrup, cookie, hooked, threats, treats",
-        #         )
-
-        #     with gr.Column():
-        #         model_selection = gr.Dropdown(
-        #             avaliable_models,
-        #             label="base",
-        #             info="Whisper models - small to large",
-        #         )
-
-        #     just_transcribe_button = gr.Button("Just Transcribe", variant="primary")
-        #     bleep_transcribe_button = gr.Button("Transcribe & Bleep", variant="primary")
-
-        # with gr.Column():
-        #     transcription_output = gr.Textbox(
-        #         label="Transcription", lines=10, show_copy_button=True
-        #     )
-        # with gr.Column():
-        #     timestamped_transcription_output = gr.Textbox(
-        #         label="Timestamped Transcription", lines=10, show_copy_button=True
-        #     )
-
-        # with gr.Column():
-        #     og_video = gr.PlayableVideo(
-        #         value=None,
-        #         show_download_button=True,
-        #         show_label=True,
-        #         label="bleeped video",
-        #     )
-
-        # with tempfile.TemporaryDirectory() as tmpdirname:
-        #     temporary_video_location = tmpdirname + "/" + "original.mp4"
-        #     temporary_audio_location = temporary_video_location.replace("mp4", "mp3")
-
-        #     bleep_video_output = temporary_video_location.replace("original", "bleep")
-        #     bleep_audio_output = bleep_video_output.replace("mp4", "mp3")
-
-        #     def download(url: str,
-        #                  tmpdirname: str) -> str | None:
-        #         if len(url) > 0:
-        #             local_file_path = download_video(url, tmpdirname)
-        #             return local_file_path
-        #         else:
-        #             return None
-
-        # just_transcribe_button.click(
-        #     just_transcribe,
-        #     inputs=[
-        #         temporary_video_location,
-        #         temporary_audio_location,
-        #         model_selection,
-        #     ],
-        #     outputs=[transcription_output, timestamped_transcription_output],
-        # )
-
-        # with gr.Column():
-        #     bleep_video = gr.PlayableVideo(
-        #         value=bleep_video_output,
-        #         show_download_button=True,
-        #         show_label=True,
-        #         label="bleeped video",
-        #     )
-
-        # just_transcribe_button.click(
-        #     just_transcribe,
-        #     inputs=[
-        #         temporary_video_location,
-        #         temporary_audio_location,
-        #         model_selection,
-        #     ],
-        #     outputs=[transcription_output, timestamped_transcription_output],
-        # )
-
-        # bleep_transcribe_button.click(
-        #     transcribe_and_bleep,
-        #     inputs=[
-        #         temporary_video_location,
-        #         temporary_audio_location,
-        #         bleep_video_output,
-        #         bleep_audio_output,
-        #         model_selection,
-        #         bleep_words,
-        #     ],
-        #     outputs=[transcription_output, timestamped_transcription_output],
-        # )
 
 if __name__ == "__main__":
     print("Launching Gradio interface...")
