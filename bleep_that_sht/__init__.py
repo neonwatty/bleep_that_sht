263714b20c36b78d262286cbef937a7bb77fb322
Jeremy Watt <jermwatt@gmail.com>
2024-11-18 15:38:38 -0700

    first push

--- bleep_that_sht/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-
-base_dir = os.path.dirname(os.path.abspath(__file__))
-main_dir = os.path.dirname(base_dir)
-
-import whisper_timestamped as whisper
-
-model = whisper.load_model("tiny", device="cpu")
-model = whisper.load_model("base", device="cpu")
--- tests/__init__.py
+++
@@ -1,6 +0,0 @@
-import os
-
-cwd = os.getcwd()
-CONTAINER_NAME = "bleep_that_sht"
-STREAMLIT_UPLOAD_FILE = "bleep_that_sht/streamlit_app_video_upload.py"
-STREAMLIT_DOWNLOAD_FILE = "bleep_that_sht/streamlit_app_url_download.py"
